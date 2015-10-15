bb877a23fb00b7aa4c8a696435a9b517421a4fea
Oleksii Rudenko <oleksii.rudenko@n-fuse.de>
2015-10-15 09:59:23 +0200

    Builder instance fix

--- test-project/main.js
+++ test-project/main.js
@@ -1,10 +1,16 @@
 import $ from 'jquery';
+import Ember from 'ember';
 import { testThing } from './test-module';
-
-testThing();
+import tmpl from './main.hbs!';
 
 $(() => {
+  testThing();
   $('body').css('background-color', 'blue');
 });
 
+var App = Ember.Application.extend({});
+Ember.TEMPLATES.index = tmpl;
+App.create({
+  rootElement: '#app'
+});
 
